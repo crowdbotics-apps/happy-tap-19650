ba1d0807dc6178d171e2f7e567ebf2aaff1e9e64
Crowdbotics <team@crowdbotics.com>
2020-08-19 10:12:36 +0000

    Initial commit.

---
+++ ios/happy_tap_19650Tests/happy_tap_19650Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface happy_tap_19650 : XCTestCase
+
+@end
+
+@implementation happy_tap_19650
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
